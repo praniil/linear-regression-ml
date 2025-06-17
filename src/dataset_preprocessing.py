c1c6605dd30ffd4ef5cb97a5bc9600a481d3aaa8
praniil <pparajuli15@gmail.com>
2025-06-17 22:48:38 +0545

    prepared the test set and train set from the dataset

--- src/dataset_preprocessing.py
+++ src/dataset_preprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 house_df = pd.read_csv('/home/pranil/python_projects/machine_learning/linear_regression/dataset/house_price_regression_dataset.csv')
 print(house_df.columns)
@@ -19,3 +20,12 @@ print(house_df.isnull().sum())
 
 print(house_df.duplicated().sum())
 house_df.drop_duplicates(inplace=True)
+
+#features and target output
+X = house_df.drop('House_Price', axis=1)
+print(X)
+y = house_df['House_Price']
+print(y)
+
+#split the dataset into test and train set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
